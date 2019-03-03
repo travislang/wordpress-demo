39d75ea0df2c0abd6049fec313b2b2da53b609b2
Travis Lang <tlang50@icloud.com>
2019-03-02 20:52:51 -0600

    fix stylesheet importing

--- wp-content/themes/wpbootstrap/functions.php
+++ wp-content/themes/wpbootstrap/functions.php
@@ -1,19 +1,16 @@
 <?php
-
+    function myprefix_load_css_and_js() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+    add_action( 'wp_enqueue_scripts', 'myprefix_load_css_and_js' );
     # theme support
     function wpb_theme_setup(){
         add_theme_support('post-thumbnails');
-
-        
     }
-
     add_action('after_setup_theme', 'wpb_theme_setup');
-
     // control except length
     function set_excerpt_length() {
         return 45;
     }
-
     add_filter('excerpt_length', 'set_excerpt_length');
-
 ?>
