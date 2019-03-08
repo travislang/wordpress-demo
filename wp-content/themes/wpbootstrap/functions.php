b721f72b322b6759a3a6fcb9154b7d034b7e1422
Travis Lang <tlang50@icloud.com>
2019-03-07 19:29:50 -0600

    add comment to fucntions.php file

--- wp-content/themes/wpbootstrap/functions.php
+++ wp-content/themes/wpbootstrap/functions.php
@@ -8,6 +8,7 @@
     function wpb_theme_setup(){
         add_theme_support('post-thumbnails');
     }
+    // adds action to hoop into wordpress core process
     add_action('after_setup_theme', 'wpb_theme_setup');
     // control except length
     function set_excerpt_length() {
