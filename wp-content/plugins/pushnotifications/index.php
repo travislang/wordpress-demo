a246594ad04933f2077f66d0b808a613db8becb5
Travis Lang <tlang50@icloud.com>
2019-03-09 14:13:10 -0600

    create file structure for plugin

---
+++ wp-content/plugins/pushnotifications/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Push Notifications
+ * Plugin URI:  https://localhost:8888
+ * Description: Basic WordPress Push Notifications  plugin test
+ * Version:     1.0
+ * Author:      Travis Lang
+ * Author URI:  https://travislang.io
+ */
+
+ // exit if accessed directly
+
+    if(!defined('ABS_PATH')){
+        exit;
+    }
+
+    echo '123';
